227e307123de425c2ad8bb363fc472cf76adce52
sergeyrybalkin <lightdelay@mail.ru>
2014-02-25 20:43:31 +0400

    initial commit

---
+++ src/main/java/Logic.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sergeyrybalkin
+ * Date: 25.02.14
+ * Time: 20:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class Logic {
+
+}
